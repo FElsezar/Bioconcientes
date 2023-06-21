3b57cbc17c3c03507952e872311bf9b64944f6d5
FElsezar <francoionelsezar@gmail.com>
2023-06-21 01:39:04 -0300

    final?

---
+++ js/blog.js
@@ -0,0 +1,159 @@
+const templates = {
+    huella: `
+    <div class="b-item">
+    <h1>¿Qué es la huella ecológica?</h1>
+    <p>El concepto "huella ecológica" surge como un indicador de sostenibilidad que trata de medir el impacto que nuestro modo de vida tiene sobre el entorno.</p>
+    <p>Todas las decisiones que como consumidores tomamos en nuestra vida cotidiana tienen un impacto sobre el planeta. Ese impacto ambiental se expresa como la cantidad de terreno biológicamente productivo que se necesita por persona para producir los recursos necesarios para mantener su estilo de vida.
+    </p>
+    <p>Según el último informe de la Global Footprint Network, con datos de 2022, la demanda actual a escala global de nuestras actividades es de 1,6 hectáreas (ha) por persona, mientras que lo que el planeta puede suministrar se sitúa en torno a las 2 ha per cápita. Además, estos valores varían según las regiones del planeta: mientras los Emiratos Arabes tiene una huella ecológica de 8,71 ha per cápita y Argentina de 3,22, Angola sólo tiene una huella ecológica de 0,86 ha por persona. Esto significa que los habitantes de los países desarrollados estamos viviendo por encima de las posibilidades de regeneración ecológica y disminuyendo cada año la capacidad planetaria de sostenernos.</p>
+    <p>Te invitamos a que <a href="https://www.footprintcalculator.org/home/es" target="_blank"><strong>calcules tu huella haciendo click aquí</strong></a></p>
+    </div>
+        `,
+    rrr: `
+    <div class="b-item">
+                <h1>¿Qué son las 3R?</h1>
+                <p>La regla de las tres erres, también conocida como las tres erres de la ecología o simplemente 3R, es
+                    una propuesta sobre hábitos de consumo. Durante la Cumbre del G8 en junio de 2004, el Primer
+                    Ministro del Japón, Koizumi Junichiro, presentó la Iniciativa, y explicó que ésta busca construir
+                    una sociedad orientada hacia el reciclaje. En abril de 2005 se llevó a cabo una asamblea de
+                    ministros en la que los representantes de más de 20 países discutieron la manera en que se podrían
+                    implementar de manera internacional acciones relacionadas a las tres erres.</p>
+                <p>Este concepto hace referencia a estrategias para el manejo de los residuos que se producen todos los
+                    días en nuestro hogar o en la industria, buscando ser más sustentables con el ambiente y reducir el
+                    volumen de basura generada.</p>
+                <div class="rrr">
+                    <h2>
+                        Re
+                        <span>
+                            <span>ducir</span>
+                            <span>ciclar</span>
+                            <span>utilizar</span>
+                            <span>ducir</span>
+                        </span>
+                    </h2>
+                </div>
+                <h2>Los beneficios que nos dan las 3Rs</h2>
+                <p>Dentro de los beneficios ambientales encontramos la disminución de residuos sólidos, el incremento de
+                    la calidad del suelo, reducción de la emisión de gases de efecto invernadero; mejorando así la
+                    calidad de aire y ayudando a que la situación del clima se estabilice.</p>
+                <p>También hay beneficios económicos, ya que los residuos sólidos pueden ser vendidos a empresas que los
+                    reciclan, ahorramos en costos en el hogar utilizando materiales reciclados. Se pueden crear
+                    diferentes artículos hechos de material reciclado como camas, sofás, sillas, mesas, lámparas,
+                    floreros, entre otros. En algunos casos, el arte de reciclar puede llegar a ser un negocio, pues se
+                    crean artículos que se venden. Por último, tenemos el beneficio social, ya que poco a poco se crea
+                    una cultura de responsabilidad con el ambiente.</p>
+                <p>Con todo esta información que tenemos ahora, antes de comprar un producto en el mercado debemos
+                    hacernos varias preguntas: ¿realmente lo necesito?, ¿este producto fue elaborado de manera que no
+                    contamine o de manera sustentable?, ¿durará mucho tiempo?, ¿contaminará al ser desechado?, ¿bajo qué
+                    circunstancias fue fabricado?, ¿existe un producto similar pero con menos empaques? La idea es que
+                    cada uno de nosotros hagamos un consumo razonado, no comprar por comprar sin mirar a quién o a qué
+                    podamos dañar.</p>
+            </div>
+        `,
+    especies: `
+    <div class="b-item">
+    <h1>Nuestra especie del día</h1>
+    <h2>Guanaco <br> <em>Lama guanicoe</em></h2>
+    <p>El guanaco es uno de los grandes herbívoros de Sudamérica y el mayor
+        de los camélidos silvestres de este continente. Esta especie manifiesta una
+        serie de adaptaciones anatómicas y fisiológicas que le permiten sobrevivir
+        en condiciones extremas (Raedeke, 1978; Franklin 1983; Puig, 1995a; De
+        Lamo, 1997; De Lamo et al., 1998). Debido a su adaptabilidad a distintas
+        condiciones, y especialmente por su forma de alimentación, el guanaco
+        ocupa hábitats con marcadas diferencias en estructura de la vegetación,
+        relieve, clima y presencia de actividades humanas.
+    </p>
+    <h2>Estado de conservación</h2>
+    <p>Las poblaciones de guanacos en Argentina declinaron continuamente desde la colonización europea hasta
+        nuestros días.</p>
+    <p>Los principales factores vinculados a esta reducción poblacional se
+        relacionan con la alteración del hábitat, la competencia por introducción de
+        ganado ovino, la caza indiscriminada -legal e ilegal- y la carencia de planes
+        de protección y manejo que asegurasen su conservación.</p>
+    <p>El manejo de guanacos en forma sustentable puede implicar cambios
+        positivos para el futuro de la especie, históricamente declinante. Los
+        beneficios obtenidos pueden favorecer una percepción positiva hacia la
+        especie, que ayude a detener su declinación e incluso, recuperar zonas de
+        su distribución original.</p>
+    <p>
+        Se estima que a fines del siglo XIX la población total de guanacos estaba
+        en el orden de los 7 millones de individuos (Cabrera y Yepes, 1960; Torres,
+        1985). Hoy, la Unión Internacional para la Conservación de la Naturaleza
+        (UICN) estima que la población total no superaría los 600.000 individuos. Un
+        relevamiento aéreo realizado en el año 2000, estimó una población mínima
+        de 402.000 animales para la Patagonia continental, -en las provincias de
+        Neuquen, Río Negro, Chubut y Santa Cruz- (Amaya et al., 2001).</p>
+    <h2>Distribución</h2>
+    <p>Actualmente, la especie ocupa sólo el 40% de su distribución original y
+        está fragmentada en poblaciones pequeñas y relativamente aisladas (Puig,
+        1995b; Franklin et al., 1997; Puig, 1992). Si bien el guanaco no se encuentra
+        amenazado de extinción a nivel continental, una serie de poblaciones tiene
+        riesgo de desaparición a nivel local e incluso regional (Cunazza et al., 1995).</p>
+    <p>En Argentina, la abundancia de guanacos es mayor en las zonas protegidas
+        –por ejemplo las Reservas La Payunia en la provincia de Mendoza (Candia
+        et al., 1993; Puig et al., 2003) y Auca Mahuida en la provincia de Neuquen
+        (Radovani et al., 2004)-, en zonas poco habitadas, de difícil acceso y en
+        ambientes de baja productividad donde no se desarrolla la actividad
+        ganadera. Si bien existen registros de poblaciones pequeñas de alta
+        densidad en áreas protegidas de la provincia de Chubut, las poblaciones
+        con densidades ecológicamente funcionales son excepcionales y están
+        separadas por grandes extensiones sin guanacos o con densidades muy
+        bajas (Baldi et al., 1997; Novaro, Funes y Walker, 2000; Baldi et al., 2001;
+        Saba et al., 2004).</p>
+    <p>
+        El objetivo central del Proyecto Guanaco es conocer el estado poblacional
+        de la especie y coordinar con las provincias en su área de distribución, la
+        actualización del Plan Nacional de Manejo a partir de datos del monitoreo de
+        variables poblacionales y ambientales. Conservar y hacer un uso sustentable
+        del guanaco a partir de la esquila en vivo de animales silvestres, con el
+        objeto de lograr fibras naturales de alta calidad, y áreas específicas para la
+        conservación.</p>
+</div>`,
+    fecha: `
+    <div class="b-item">
+    <h1>Fecha para el calendario</h1>
+    <h2>Día Internacional de la Preservación de la Capa de ozono</h2>
+    <h3>16 de septiembre</h3>
+    <p>La capa de ozono es una franja frágil de gas que protege la Tierra de los efectos nocivos de los rayos solares, contribuyendo así a preservar la vida en el planeta.</p>
+    <p>El año pasado se cumplieron 35 años de la adopción del Protocolo de Montreal, uno de los acuerdos medioambientales de mayor éxito, que puso remedio a una de las mayores amenazas a las que se había enfrentado la humanidad: el agotamiento de la capa de ozono.</p>
+    <p>La utilización durante años de ciertos productos químicos había dañado la capa, poniendo en peligro la existencia del ser humano y la del resto de seres vivos del planeta. Un esfuerzo internacional conjunto permitió la eliminación y reducción del uso de sustancias que agotaban la capa de ozono, ayudando no solo a protegerla para la generación actual y las venideras, sino también a mejorar los resultados de las iniciativas dirigidas a afrontar al cambio climático. Esos esfuerzos han protegido asímismo la salud humana y los ecosistemas reduciendo la radiación ultravioleta del sol que llega a la Tierra.</p>
+    <p>Con este acuerdo, se demostró que el multilateralismo y la cooperación mundial efectiva funcionan. Ahora, la capa de ozono se recupera poco a poco, permitiendo que siga protegiendo a la humanidad de la radiación ultravioleta del sol.</p>
+</div>`,
+}
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        view: 'rrr',
+    },
+    components: {
+        huella: {
+            data: function () {
+                return {}
+            },
+            props: [],
+            template: templates.huella
+        },
+        rrr: {
+            data: function () {
+                return {}
+            },
+            props: [],
+            template: templates.rrr
+        },
+        especies: {
+            data: function () {
+                return {}
+            },
+            props: [],
+            template: templates.especies
+        },
+        fecha: {
+            data: function () {
+                return {}
+            },
+            props: [],
+            template: templates.fecha
+        },
+    }
+})
